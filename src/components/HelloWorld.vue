8c2fa481fecac7c7694237ca0a0a946e99213711
whc <1532106001@qq.com>
2019-04-08 15:06:50 +0800

    Enter

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+ <span id="span">{{msg}}</span>
+</template>
+
+<script>
+export default {
+   name:"span",
+   msg:"123"
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
